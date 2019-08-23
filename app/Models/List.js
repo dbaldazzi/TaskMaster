787aaa975045f19ec2f43fbef9aa122a7662b72b
dbaldazzi <d.baldazzi@hotmail.com>
2019-08-23 12:14:37 -0600

    added template to lists.js

--- app/Models/List.js
+++ app/Models/List.js
@@ -1,4 +1,24 @@
 export default class List {
     //TODO You will need to create a constructor 
     //and the methods needed to create the view template for this model
+    constructor(data) {
+        this._id = data.id,
+            this.name = data.name
+
+    }
+
+    getTemplate(index) {
+        let template = `
+ <div class="col-r border">
+ <h1>${this.name}</h1> 
+ <ul>`
+        template += this.drawitems(index)
+            < form >
+            <div class="form-group">
+                <label for="Items">Items</label>
+                <input type="list" class="form-control" id="items" aria-describedby="items" placeholder="items">
+                    <small id="emailHelp" class="form-text text-muted">List items.</small>
+  </div>
+                `
+                }
 }
